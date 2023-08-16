b771ca837aea8a50a9e629a878faf8e546c2da4d
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-08-16 01:43:01 -0300

    ✬ feat: update handler structure to separate subpackage

--- routers/routes.go
+++ routers/routes.go
@@ -1,44 +1,18 @@
 package router
 
 import (
-	"net/http"
+	"github.com/Lucasvmarangoni/go-api/handler"
 	"github.com/gin-gonic/gin"
 )
 
-func initializeRoutes(r *gin.Engine){	
+func initializeRoutes(r *gin.Engine) {
 
 	v1 := r.Group("/api/v1")
 	{
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET Opening",
-			})
-		})
-
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET Opening",
-			})
-		})
-
-		v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET Opening",
-			})
-		})
-
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET Opening",
-			})
-		})
-
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "GET Opening",
-			})
-		})
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
-
-	
 }
